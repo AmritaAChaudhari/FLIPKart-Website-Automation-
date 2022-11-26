abe788dcad4949ead62c967f2aba33052013bf16
AmritaAChaudhari <93758159+AmritaAChaudhari@users.noreply.github.com>
2022-11-26 17:28:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-// program to check if a number is prime or not
+const password = document.querySelector('input[type="password"]');
 
-// take input from the user
-const number = parseInt(prompt("Enter a positive number: "));
-let isPrime = true;
+password.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';
+});
 
-// check if number is equal to 1
-if (number === 1) {
-    console.log("1 is neither prime nor composite number.");
-}
-
-// check if number is greater than 1
-else if (number > 1) {
-
-    // looping through 2 to number-1
-    for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
-        console.log(`${number} is a prime number`);
-    } else {
-        console.log(`${number} is a not prime number`);
-    }
-}
-
-// check if number is less than 1
-else {
-    console.log("The number is not a prime number.");
-}
+password.addEventListener('blur', (event) => {
+  event.target.style.background = '';
+});
