592f2fb702f180131549a1daa7a661ecf296c1fa
AmritaAChaudhari <93758159+AmritaAChaudhari@users.noreply.github.com>
2023-02-01 11:16:20 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const password = document.querySelector('input[type="password"]');
-
-password.addEventListener('focus', (event) => {
-  event.target.style.background = 'pink';
-});
-
-password.addEventListener('blur', (event) => {
-  event.target.style.background = '';
-});
